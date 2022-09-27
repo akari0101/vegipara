d41055e59b78581bffdfad9ef13fb1c3bb2c8e93
EC2 Default User <ec2-user@ip-172-31-43-203.ec2.internal>
2022-09-27 11:41:43 +0000

    migration外部キー制約

--- db/migrate/20220922064410_create_tagmaps.rb
+++ db/migrate/20220922064410_create_tagmaps.rb
@@ -1,8 +1,8 @@
 class CreateTagmaps < ActiveRecord::Migration[6.1]
   def change
     create_table :tagmaps do |t|
-      t.references :item, null: false, foreign_key: true
-      t.references :tag, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true, type: :integer
+      t.references :tag, null: false, foreign_key: true, type: :integer
 
       t.timestamps
     end
